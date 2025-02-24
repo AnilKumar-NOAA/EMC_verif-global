ba1d9f561fd000592aa9969bb3de7f198304044a
AnilKumar-NOAA <Anil.Kumar@gaea53.ncrc.gov>
2025-02-24 11:37:25 -0500

    Gaea C6 Dir path updated

--- ush/run_verif_global_in_global_workflow.sh
+++ ush/run_verif_global_in_global_workflow.sh
@@ -282,9 +282,9 @@ elif [ $machine = "GAEAC5" ]; then
     export prepbufr_arch_dir="/gpfs/f5/ufs-ard/world-shared/global/metplus.data/prepbufr"
     export ccpa_24hr_arch_dir="/gpfs/f5/ufs-ard/world-shared/global/metplus.data/obdata/ccpa_accum24hr"
 elif [ $machine = "GAEAC6" ]; then
-    export global_archive="/gpfs/f5/ufs-ard/world-shared/global/metplus.data/archive"
-    export prepbufr_arch_dir="/gpfs/f5/ufs-ard/world-shared/global/metplus.data/prepbufr"
-    export ccpa_24hr_arch_dir="/gpfs/f5/ufs-ard/world-shared/global/metplus.data/obdata/ccpa_accum24hr"
+    export global_archive="/gpfs/f6/bil-fire8/world-shared/global/metplus.data/archive"
+    export prepbufr_arch_dir="/gpfs/f6/bil-fire8/world-shared/global/metplus.data/prepbufr"
+    export ccpa_24hr_arch_dir="/gpfs/f6/bil-fire8/world-shared/global/metplus.data/obdata/ccpa_accum24hr"
 elif [ $machine = "ORION" -o $machine = "HERCULES" ]; then
     export global_archive="/work/noaa/ovp/mrow/archive"
     export prepbufr_arch_dir="/work/noaa/ovp/mrow/prepbufr"
